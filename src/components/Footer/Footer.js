ad01fa213fc48926925351b50877873ec003e207
Mohini kumari <mohini12062001@gmail.com>
2022-08-21 15:16:36 +0530

    initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div>
+       <section className="contactme bg-dark text-white " id="contactme">
+       <div className="containercontact">
+    
+        <h1>Contact me</h1>
+       <div><i className="bi-mailbox pe-2"> mohini12062001@gmail.com</i> </div>
+        <p><i className="bi-phone pe-2"> 7480833963</i> </p>
+        </div>
+<hr></hr>
+           <h5 className = "copyright">Copyright© Mohini</h5>
+
+    </section>
+    </div>
+  )
+}
+
